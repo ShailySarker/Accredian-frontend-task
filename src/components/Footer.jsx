bf67f368a7f0ba29a7d2e21ecdb71c838a0a8285
ShailySarker <shailysarker99@gmail.com>
2025-02-13 21:28:53 +0600

    Header section added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import companyLogo from '../../src/assets/Image/Shared/Footer_companyLogo.png';
+import logo1 from '../../src/assets/Image/Shared/Footer_logo1.png';
+import logo2 from '../../src/assets/Image/Shared/Footer_logo2.png';
+import logo3 from '../../src/assets/Image/Shared/Footer_logo3.png';
+import logo4 from '../../src/assets/Image/Shared/Footer_logo4.png';
+import logo5 from '../../src/assets/Image/Shared/Footer_logo5.png';
+import link from '../../src/assets/Image/Shared/Footer_link.png';
 
 const Footer = () => {
     return (
-        <div>
-            footer
+        <div className='bg-[#282828]'>
+            
         </div>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,7 +56,7 @@ const Header = () => {
                 </div>
                 {/* medium and small device menu */}
                 <div className="lg:hidden visible flex items-center md:gap-3 gap-2">
-                <select className='lg:hidden visible bg-[#1A73E8] md:py-[6px] py-1 text-white rounded-md px-2 w-[90px] cursor-pointer'>
+                    <select className='lg:hidden visible bg-[#1A73E8] md:py-[6px] py-1 text-white rounded-md px-2 w-[90px] cursor-pointer'>
                         <option defaultValue>Explore</option>
                         <option value="Product Management">Product Management</option>
                         <option value="Strategy & Leadership">Strategy & Leadership</option>
@@ -74,24 +74,20 @@ const Header = () => {
             </div>
             {/* medium and small device menu */}
             {isMobileMenuOpen && (
-                <div style={{ zIndex: 9999 }} className="z-50 lg:hidden absolute md:top-[90px] top-[70px] right-4 px-4 md:py-6 py-4 md:w-52 w-44 rounded-t-xl rounded-br-xl rounded-bl-[50px] shadow-lg border-4 border-[#A21D3C] bg-[#A21D3C] text-white">
-                    <ul className="md:mb-4 mb-3 md:text-[16.7px] text-[15.6px] font-semibold">
-                        <NavLink to='/' className={({ isActive }) => isActive ? "border-b-2 border-white px-[2px]" : "px-[2px]"} onClick={closeDropdown}>Home</NavLink>
-                    </ul>
-                    <ul className="md:mb-4 mb-3 md:text-[16.7px] text-[15.6px] font-semibold">
-                        <NavLink to='/aboutUs' className={({ isActive }) => isActive ? "border-b-2 border-white px-[2px]" : "px-[2px]"} onClick={closeDropdown}>About Us</NavLink>
-                    </ul>
-                    <ul className="md:mb-4 mb-3 md:text-[16.7px] text-[15.6px] font-semibold">
-                        <NavLink to='/services' className={({ isActive }) => isActive ? "border-b-2 border-white px-[2px]" : "px-[2px]"} onClick={closeDropdown}>Services</NavLink>
-                    </ul>
-                    <ul className="md:mb-4 mb-3 md:text-[16.7px] text-[15.6px] font-semibold">
-                        <NavLink to='/blog' className={({ isActive }) => isActive ? "border-b-2 border-white px-[2px]" : "px-[2px]"} onClick={closeDropdown}>Blog</NavLink>
-                    </ul>
-                    <ul className="md:mb-4 mb-3 md:text-[16.7px] text-[15.6px] font-semibold">
-                        <NavLink to='/contactUs' className={({ isActive }) => isActive ? "border-b-2 border-white px-[2px]" : "px-[2px]"} onClick={closeDropdown}>Contact Us</NavLink>
-                    </ul>
-                    <button className="md:mt-1 mt-[2px] md:mb-3 mb-2 bg-[#AD3853] text-[#F3EFEF] font-medium w-full md:py-2 py-[6px] rounded-[34px]">LogIn</button>
-                    <button className="bg-[#AD3853] text-[#F3EFEF] font-medium w-full md:py-2 py-[6px] rounded-[34px]">SignUp</button>
+                <div style={{ zIndex: 9999 }} className="z-50 lg:hidden absolute md:top-[120px] top-[125px] md:right-10 right-4 px-4 md:py-6 py-4 md:w-44 w-40 rounded-t-xl rounded-br-xl rounded-bl-[50px] shadow-lg border-4 border-[#1A73E8] bg-white">
+                    <div className='flex flex-col items-start md:gap-3 gap-2'>
+                        <p className='text-[#1A202C] font-medium md:text-sm text-[13.8px] cursor-pointer'>Refer & Earn</p>
+                        <p className='text-[#1A202C] font-medium md:text-sm text-[13.8px] cursor-pointer'>Resources</p>
+                        <p className='text-[#1A202C] font-medium md:text-sm text-[13.8px] cursor-pointer'>About Us</p>
+                        <div className='flex flex-col items-start md:gap-2 gap-[7px]'>
+                            <Link to="https://accredian.com/login/">
+                                <button className='bg-[#94A3B833] md:py-[6px] py-1 rounded-md px-4 text-[#1A202C] font-medium md:text-sm text-[13.8px]'>Login</button>
+                            </Link>
+                            <Link to="https://trial.accredian.com/">
+                                <button className='bg-[#1A73E8] md:py-[6px] py-1 text-white rounded-md px-4 font-medium md:text-sm text-[13.8px]'>Try for free</button>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             )}
         </div>
